f99ef7e30897344277d8dae2505ca5694a56c8fc
RF\CARMELHE <hechtcarmel@gmail.com>
2022-06-19 10:49:56 +0300

    feat: trying to ask nicely

--- 1/main.py
+++ 1/main.py
@@ -3,8 +3,12 @@
 MAX_COURSE_PRICE = 100
 courses = []
 
-def get_manager_validation(course_price):
-    return course_price.isdigit() and int(course_price) <= MAX_COURSE_PRICE
+def get_manager_validation(course_price, ask_nicely):
+    max_price = MAX_COURSE_PRICE
+    if ask_nicely:
+        max_price-=20
+    return course_price.isdigit() and int(course_price) <= max_price
+
 
 def main():
     print("Welcome to the Course Registration System")
@@ -12,10 +16,15 @@ def main():
         course_name = input("Enter a course name: ")
         course_price = input("Enter a course price: ")
 
-        if get_manager_validation(course_price):
+        if get_manager_validation(course_price, ask_nicely=False):
             courses.append(course_name)
         else:
-            print("Sorry, but the course is too expensive!")
+            print("Course is too expensive, trying to ask nicely...")
+            if get_manager_validation(course_price, ask_nicely=True):
+                print("Success!")
+                courses.append(course_name) 
+            else:
+                print("Sorry, but the course is too expensive!")
             
         print("Courses: ", courses, "\n")
 
