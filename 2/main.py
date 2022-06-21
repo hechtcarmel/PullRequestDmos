fc5c3e3e198bd38f7587e4e11e6885cfc1c42dd9
hechtcarmel <36325296+hechtcarmel@users.noreply.github.com>
2022-06-21 17:09:25 +0300

    fix: better grammer

--- 2/main.py
+++ 2/main.py
@@ -11,8 +11,8 @@ def get_manager_validation(course_price, ask_nicely):
 def main():
     print("Welcome to the Course Registration Systen")
     while True:
-        course_name = input("Enter aa course name: ")
-        course_price = input("Enter aa course price: ")
+        course_name = input("Enter the course name: ")
+        course_price = input("Enter the course price: ")
 
         if get_manager_validation(course_price, ask_nicely=False):
             courses.append(course_name)
