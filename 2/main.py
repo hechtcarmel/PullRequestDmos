df13abfb6895f7564dab0c99b774db44696a1c6f
RF\CARMELHE <hechtcarmel@gmail.com>
2022-06-21 16:56:45 +0300

    fix: better grammer

--- 2/main.py
+++ 2/main.py
@@ -11,8 +11,8 @@ def get_manager_validation(course_price, ask_nicely):
 def main():
     print("Welcome to the Course Registration System")
     while True:
-        course_name = input("Enter a course name: ")
-        course_price = input("Enter a course price: ")
+        course_name = input("Enter the course name: ")
+        course_price = input("Enter the course price: ")
 
         if get_manager_validation(course_price, ask_nicely=False):
             courses.append(course_name)
